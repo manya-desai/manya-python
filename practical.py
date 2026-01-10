42c0c6191af54e4b4a246f2e0d304a5ab81b92dc
Manya Desai <manyadesai30@gmail.com>
2026-01-10 09:11:32 +0530

    Add files via upload

---
+++ practical.py
@@ -0,0 +1,23 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Hello , Welcoming to Python Programming!")
+Hello , Welcoming to Python Programming!
+>>> a = int(input("Enter a number :"))
+Enter a number :20
+>>> b = int(input("Enter a number :"))
+Enter a number :20
+>>> sum = a + b
+>>> print("Sum =",sum)
+Sum = 40
+>>> num = int(input("Enter a number :"))
+Enter a number :30
+>>> if num % 2 ==0:
+...     print("THE NUMBER IS EVEN")
+... else:
+...     print("THE NUMBER IS ODD")
+... 
+...     
+THE NUMBER IS EVEN
+>>> year = int(input("Enter a year :"))
+Enter a year :2027
+>>> 
