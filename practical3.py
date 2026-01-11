0ff0494c0a48eb3676ae4fae0ff06a1c35119afb
Manya Desai <manyadesai30@gmail.com>
2026-01-11 20:25:33 +0530

    Add files via upload

---
+++ practical3.py
@@ -0,0 +1,16 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> num = int(input("Enter a number :"))
+Enter a number :30
+>>> square = num * num
+>>> print("Square =",square)
+Square = 900
+>>> radius = float(input("Enter Radius :"))
+Enter Radius :20
+>>> area = 3.14 * radius * radius
+>>> print("Area of circle =",area)
+Area of circle = 1256.0
+>>> value = input("Enter any value :")
+Enter any value :20
+>>> print("Data type is :",type(value))
+Data type is : <class 'str'>
