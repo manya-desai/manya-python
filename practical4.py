f9605c64ebb41e31868162ccfdf19468115c71bb
Manya Desai <manyadesai30@gmail.com>
2026-01-11 20:26:54 +0530

    Add files via upload

---
+++ practical4.py
@@ -0,0 +1,16 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> import math
+>>> num = int(input("Enter a number :"))
+Enter a number :20
+>>> print("Square root =",math.sqrt(num))
+Square root = 4.47213595499958
+>>> print("Factorial =",math.factorial(num))
+Factorial = 2432902008176640000
+>>> base = int(input("Enter a base :"))
+Enter a base :40
+>>> power = int(input("Enter a power :"))
+Enter a power :2
+>>> result = base ** power
+>>> print("Result =",result)
+Result = 1600
