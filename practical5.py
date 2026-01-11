4312022256f039ab083aeb2180531fd131bdfaa1
Manya Desai <manyadesai30@gmail.com>
2026-01-11 20:27:33 +0530

    Add files via upload

---
+++ practical5.py
@@ -0,0 +1,13 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> num = int(input("Enter a number :"))
+Enter a number :20
+>>> if num > 0:
+...     print("POSITIVE NUMBER")
+... elif num < 0:
+...     print("NEGATIVE NUMBER")
+... else:
+...     print("ZERO")
+... 
+...     
+POSITIVE NUMBER
