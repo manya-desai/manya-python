c4d1718ec53d1be23f477a56e88f8f0be26fbfda
Manya Desai <manyadesai30@gmail.com>
2026-01-13 11:00:50 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,22 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> p = float(input("Enter principal :"));
+Enter principal :2.30
+>>> r = float(input("Enter rate : "));
+Enter rate : 20
+>>> t = float(input("Enter Time : "));
+Enter Time : 20
+>>> si = (p * r * t)/100
+>>> print("Simple interest =",si)
+Simple interest = 9.2
+>>> a = int(input("Enter First number :"));
+Enter First number :20
+>>> b = int(input("Enter Second number :"));
+Enter Second number :20
+>>> if a > b:
+...     print("Maximum is",a)
+... else :
+...     print("Maximum is",b)
+... 
+...     
+Maximum is 20
