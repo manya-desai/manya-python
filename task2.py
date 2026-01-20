6686544bc1a5a01f6dadc2c285b694c6ea7c5690
Manya Desai <manyadesai30@gmail.com>
2026-01-20 12:38:20 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,15 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> for i in range(1,6):
+...     print(i)
+... 
+...     
+1
+2
+3
+4
+5
+>>> s = input("Enter a string :")
+Enter a string :12
+>>> print("Length of string =",len(s))
+Length of string = 2
