194f6d239fac731288e46e0a1a00770ec17b6a3f
Manya Desai <manyadesai30@gmail.com>
2026-01-27 08:05:25 +0530

    Add files via upload

---
+++ operator3.py
@@ -0,0 +1,14 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Boolean (Boolean Values)
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> 
+>>> #Bitwise Operator
+>>> a=5
+>>> b=3
+>>> print(a & b)
+1
+>>> print(a | b)
+7
