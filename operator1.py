d3a83cf02b2f16e91b8c67047d43dab51dc389a3
Manya Desai <manyadesai30@gmail.com>
2026-01-27 08:04:43 +0530

    Add files via upload

---
+++ operator1.py
@@ -0,0 +1,21 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # Arithmetic Operator
+>>> a=5
+>>> b=10
+>>> print(a+b)
+15
+>>> print(a-b)
+-5
+>>> print(a/b)
+0.5
+>>> 
+>>> #Assignment Operator
+>>> x=10
+>>> print(x)
+10
+>>> 
+>>> #Unary Minus
+>>> a=5
+>>> print(-a)
+-5
