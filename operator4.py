f5ab8af622cdaccaa1bb09f902efc568c8be0f57
Manya Desai <manyadesai30@gmail.com>
2026-01-27 08:05:44 +0530

    Add files via upload

---
+++ operator4.py
@@ -0,0 +1,16 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Membership Operator
+>>> number=[1,2,3,4,5]
+>>> print(2 in number)
+True
+>>> print(5 not in number)
+False
+>>> 
+>>> #Identity Operator
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
