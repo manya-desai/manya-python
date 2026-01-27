643ab256885e834c9d92576f4338762d3d065927
Manya Desai <manyadesai30@gmail.com>
2026-01-27 08:05:02 +0530

    Add files via upload

---
+++ operator2.py
@@ -0,0 +1,20 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Relational Operator
+>>> p=10
+>>> q=5
+>>> print(p>q)
+True
+>>> print(p==q)
+False
+>>> 
+>>> 
+>>> #Logical Operator
+>>> x=True
+>>> y=False
+>>> print(x and y)
+False
+>>> print(x or y)
+True
+>>> print(not x)
+False
