ae415ccb4a7a15ae3803c3e1d46d69e614d355e5
Manya Desai <manyadesai30@gmail.com>
2026-01-27 08:04:21 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,14 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a = int(input("Enter First number :"))
+Enter First number :50
+>>> b = int(input("Enter Second number :"))
+Enter Second number :20
+>>> c = int(input("Enter Third number :"))
+Enter Third number :30
+>>> print("Sum =", a + b + c)
+Sum = 100
+>>> name = input("Enter your name :")
+Enter your name :MANYA
+>>> print("HELLO",name)
+HELLO MANYA
