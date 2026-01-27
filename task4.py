1951976b1f823093985776fbe374075a586014f6
Manya Desai <manyadesai30@gmail.com>
2026-01-27 08:03:55 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,13 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> n = int(input("Enter a number :"))
+Enter a number :-39
+>>> if n > 0:
+...     print("POSITIVE NUMBER")
+... elif n < 0:
+...     print("NEGATIVE NUMBER")
+... else :
+...     print("ZERO")
+... 
+...     
+NEGATIVE NUMBER
