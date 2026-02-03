3ed4ee84b376c538e4f390c8224bedf949edccac
Manya Desai <manyadesai30@gmail.com>
2026-02-03 12:25:54 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age :"))
+if age >= 18:
+        print("Yes you are eligible")
+else:
+        print("No you are not eligible")
+
+
+marks = int(input("Enter your marks :"))
+if marks >= 90:
+        print("you will get A grade")
+elif marks >= 70:
+        print("you will get B grade") 
+else:
+        print("you will get C grade")         
