0c074b6e52bcc00f3d30a47658eb22cd40066ba2
Manya Desai <manyadesai30@gmail.com>
2026-02-20 19:19:11 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,27 @@
+""" 1. Print numbers 1 to 5
+i=1
+while i<=5:
+    print(i)
+    i=i+1 """
+
+""" 2.Print numbers 1 to 10    
+i=1
+while i<=10:
+    print(i)
+    i=i+1 """
+
+""" 3. Sum of First n Natural numbers
+n=int(input("Enter Number : "))
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+print("sum = ",s) """
+
+""" 4. Table of a Number """
+num=int(input("Enter Number : "))
+i=1
+while i<=10:
+    print(num , " x " , i , " = " , num*i)
+    i=i+1 
